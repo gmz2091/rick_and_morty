adf4abd47d512bf148ee0525f638c7f6fe8a3757
Magdiel <mgomez2091@gmail.com>
2023-06-08 15:09:47 -0700

    Initial Commit

---
+++ testingApp/testingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingAppApp.swift
+//  testingApp
+//
+//  Created by Magdiel G. on 08/06/23.
+//
+
+import SwiftUI
+
+@main
+struct testingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
